64ff70d2c508585de8f608ecc56ff3fe8899d088
tuanle1293 <levantuan1293@gmail.com>
2022-10-28 09:50:19 +0700

    add search result

---
+++ wordpress/wp-content/themes/twentytwenty/header.php
@@ -0,0 +1,194 @@
+<?php
+
+/**
+ * Header file for the Twenty Twenty WordPress default theme.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+?>
+<!DOCTYPE html>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<html class="no-js" <?php language_attributes(); ?>>
+
+<head>
+
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+
+</head>
+
+<body <?php body_class(); ?>>
+
+	<?php
+	wp_body_open();
+	?>
+
+	<header id="site-header" class="header-footer-group">
+
+		<div class="header-inner section-inner">
+
+			<div class="header-titles-wrapper">
+
+				<?php
+
+				// Check whether the header search is activated in the customizer.
+				$enable_header_search = get_theme_mod('enable_header_search', true);
+
+				if (true === $enable_header_search) {
+
+				?>
+
+					<button class="toggle search-toggle mobile-search-toggle" data-toggle-target=".search-modal" data-toggle-body-class="showing-search-modal" data-set-focus=".search-modal .search-field" aria-expanded="false">
+						<span class="toggle-inner">
+							<span class="toggle-icon">
+								<?php twentytwenty_the_theme_svg('search'); ?>
+							</span>
+							<span class="toggle-text"><?php _ex('Search', 'toggle text', 'twentytwenty'); ?></span>
+						</span>
+					</button><!-- .search-toggle -->
+
+				<?php } ?>
+
+				<div class="header-titles">
+
+					<?php
+					// Site title or logo.
+					twentytwenty_site_logo();
+
+					// Site description.
+					twentytwenty_site_description();
+
+					//Add search in menu
+					get_search_form();
+					?>
+
+				</div><!-- .header-titles -->
+
+				<button class="toggle nav-toggle mobile-nav-toggle" data-toggle-target=".menu-modal" data-toggle-body-class="showing-menu-modal" aria-expanded="false" data-set-focus=".close-nav-toggle">
+					<span class="toggle-inner">
+						<span class="toggle-icon">
+							<?php twentytwenty_the_theme_svg('ellipsis'); ?>
+						</span>
+						<span class="toggle-text"><?php _e('Menu', 'twentytwenty'); ?></span>
+					</span>
+				</button><!-- .nav-toggle -->
+
+			</div><!-- .header-titles-wrapper -->
+
+			<div class="header-navigation-wrapper">
+
+				<?php
+				if (has_nav_menu('primary') || !has_nav_menu('expanded')) {
+				?>
+
+					<nav class="primary-menu-wrapper" aria-label="<?php echo esc_attr_x('Horizontal', 'menu', 'twentytwenty'); ?>">
+
+						<ul class="primary-menu reset-list-style">
+
+							<?php
+							if (has_nav_menu('primary')) {
+
+								wp_nav_menu(
+									array(
+										'container'  => '',
+										'items_wrap' => '%3$s',
+										'theme_location' => 'primary',
+									)
+								);
+							} elseif (!has_nav_menu('expanded')) {
+
+								wp_list_pages(
+									array(
+										'match_menu_classes' => true,
+										'show_sub_menu_icons' => true,
+										'title_li' => false,
+										'walker'   => new TwentyTwenty_Walker_Page(),
+									)
+								);
+							}
+							?>
+
+						</ul>
+
+					</nav><!-- .primary-menu-wrapper -->
+				<?php
+				}
+
+				if (true === $enable_header_search || has_nav_menu('expanded')) {
+				?>
+
+					<div class="header-toggles hide-no-js">
+
+						<?php
+						if (has_nav_menu('expanded')) {
+						?>
+
+							<div class="toggle-wrapper nav-toggle-wrapper has-expanded-menu">
+
+								<button class="toggle nav-toggle desktop-nav-toggle" data-toggle-target=".menu-modal" data-toggle-body-class="showing-menu-modal" aria-expanded="false" data-set-focus=".close-nav-toggle">
+									<span class="toggle-inner">
+										<span class="toggle-text"><?php _e('Menu', 'twentytwenty'); ?></span>
+										<span class="toggle-icon">
+											<?php twentytwenty_the_theme_svg('ellipsis'); ?>
+										</span>
+									</span>
+								</button><!-- .nav-toggle -->
+
+							</div><!-- .nav-toggle-wrapper -->
+						<?php
+						}
+
+						if (true === $enable_header_search) {
+						?>
+
+							<div class="toggle-wrapper search-toggle-wrapper">
+
+								<button class="toggle search-toggle desktop-search-toggle" data-toggle-target=".search-modal" data-toggle-body-class="showing-search-modal" data-set-focus=".search-modal .search-field" aria-expanded="false">
+									<span class="toggle-inner">
+										<?php twentytwenty_the_theme_svg('search'); ?>
+										<span class="toggle-text"><?php _ex('Search', 'toggle text', 'twentytwenty'); ?></span>
+									</span>
+								</button><!-- .search-toggle -->
+
+							</div>
+							<!-- Add icon user -->
+						<p>Add icon</p>
+
+						<?php
+						}
+						?>
+
+					</div><!-- .header-toggles -->
+				<?php
+				}
+				?>
+
+			</div><!-- .header-navigation-wrapper -->
+
+		</div><!-- .header-inner -->
+
+		<?php
+		// Output the search modal (if it is activated in the customizer).
+		if (true === $enable_header_search) {
+			get_template_part('template-parts/modal-search');
+		}
+		?>
+
+	</header><!-- #site-header -->
+
+	<?php
+	// Output the menu modal.
+	get_template_part('template-parts/modal-menu');
---
+++ wordpress/wp-content/themes/twentytwenty/template-parts/content.php
@@ -0,0 +1,203 @@
+<?php
+
+/**
+ * The default template for displaying content
+ *
+ * Used for both singular and index.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+?>
+<?php
+if (!is_single()) { ?>
+	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+		<?php
+		// get_template_part('template-parts/entry-header');
+
+		// if (!is_search()) {
+		// 	get_template_part('template-parts/featured-image');
+		// }
+		?>
+
+		<div class="post-inner <?php echo is_page_template('templates/template-full-width.php') ? '' : 'thin'; ?> ">
+
+			<div class="entry-content">
+				<?php
+				if (is_search()) { ?>
+					<?php $post = get_post(get_the_ID());
+					$day = date('d', strtotime($post->post_date));
+					$month = date('m', strtotime($post->post_date));
+					$year = date('y', strtotime($post->post_date));
+					$title = $post->post_title;
+					$content = $post->post_content;
+					?>
+					<div class="list_search_detail">
+						<div class="left_search_detail">
+						<?php echo get_the_post_thumbnail() ?>
+						</div>
+						<div class="middle_search_detail">
+							<div class="date">
+							<?php
+							echo $day;
+							?>
+							</div>
+							<div class="month">
+					<?php echo "Tháng" . $month; ?>
+							</div>
+						</div>
+						<div class="right_search_detail">
+							<div class="title_search_detail">
+							<?php
+							echo $title;
+							?>
+							</div>
+							<div class="content_search_detail">
+								<?php 	echo $content ?>
+							</div>
+						</div>
+					</div>
+				<?php
+				}
+				?>
+
+				<?php
+				if (is_search() || !is_singular() && 'summary' === get_theme_mod('blog_content', 'full')) {
+					// the_excerpt();		
+				} else {
+					the_content(__('Continue reading', 'twentytwenty'));
+				}
+				?>
+
+
+
+			</div><!-- .entry-content -->
+
+		</div><!-- .post-inner -->
+
+		<div class="section-inner">
+			<?php
+			wp_link_pages(
+				array(
+					'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__('Page', 'twentytwenty') . '"><span class="label">' . __('Pages:', 'twentytwenty') . '</span>',
+					'after'       => '</nav>',
+					'link_before' => '<span class="page-number">',
+					'link_after'  => '</span>',
+				)
+			);
+
+			edit_post_link();
+
+			// Single bottom post meta.
+			twentytwenty_the_post_meta(get_the_ID(), 'single-bottom');
+
+			if (post_type_supports(get_post_type(get_the_ID()), 'author') && is_single()) {
+				get_template_part('template-parts/entry-author-bio');
+			}
+			?>
+
+		</div><!-- .section-inner -->
+
+		<?php
+
+		if (is_single()) {
+			get_template_part('template-parts/navigation');
+		}
+
+		/*
+	 * Output comments wrapper if it's a post, or if comments are open,
+	 * or if there's a comment number – and check for password.
+	 */
+		if ((is_single() || is_page()) && (comments_open() || get_comments_number()) && !post_password_required()) {
+		?>
+
+			<div class="comments-wrapper section-inner">
+				<?php comments_template(); ?>
+
+			</div><!-- .comments-wrapper -->
+
+		<?php
+		}
+		?>
+
+	</article><!-- .post -->
+	<!-- Trang detail post -->
+<?php
+} else { ?>
+	<?php
+	$post = get_post(get_the_ID());
+	?>
+	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+		<div class="post-inner <?php echo is_page_template('templates/template-full-width.php') ? '' : 'thin'; ?> ">
+			<div class="entry-content">
+				<div class="detail-content" style="display: flex; max-width:100%;">
+					<div class="left_entry-content" style="width:30%;">
+						<h1>Categories</h1>
+					</div>
+					<div class="middle_entry-content" style="width:40%;">
+						<?php echo $post->post_title; ?>
+						<?php echo date('d', strtotime($post->post_date)); ?>
+						<?php echo date('m', strtotime($post->post_date)); ?>
+						<?php echo $post->post_content; ?>
+					</div>
+					<div class="right_entry-content" style="width:30%;">
+						<h1>Last Coment</h1>
+					</div>
+				</div>
+
+			</div>
+		</div>
+
+	</article><!-- .post -->
+	<div class="section-inner">
+		<?php
+		wp_link_pages(
+			array(
+				'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__('Page', 'twentytwenty') . '"><span class="label">' . __('Pages:', 'twentytwenty') . '</span>',
+				'after'       => '</nav>',
+				'link_before' => '<span class="page-number">',
+				'link_after'  => '</span>',
+			)
+		);
+
+		edit_post_link();
+
+		// Single bottom post meta.
+		twentytwenty_the_post_meta(get_the_ID(), 'single-bottom');
+
+		if (post_type_supports(get_post_type(get_the_ID()), 'author') && is_single()) {
+			// ĐOạn trong này
+			get_template_part('template-parts/entry-author-bio');
+		}
+		?>
+
+	</div><!-- .section-inner -->
+
+	<?php
+
+	if (is_single()) {
+		get_template_part('template-parts/navigation');
+	}
+
+	/*
+	 * Output comments wrapper if it's a post, or if comments are open,
+	 * or if there's a comment number – and check for password.
+	 */
+	if ((is_single() || is_page()) && (comments_open() || get_comments_number()) && !post_password_required()) {
+	?>
+
+		<div class="comments-wrapper section-inner">
+			<?php comments_template(); ?>
+
+		</div><!-- .comments-wrapper -->
+
+<?php
+	}
+}
+?>
